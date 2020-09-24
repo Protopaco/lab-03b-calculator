3d25c98cf9d4775e195841116fb6df28e1d1cf9a
Paul Stevens <twinkisan@hotmail.com>
2020-09-24 11:39:04 -0700

    adds comments to JavaScript

--- app.js
+++ app.js
@@ -10,30 +10,31 @@ calcbutton.addEventListener('click', () => {
     //creates anwser box
     answer.style.backgroundColor = '#ac4b1c';
 
+    //converts inputs into Numbers
     let num1 = Number(input1.value);
     let num2 = Number(input2.value);
 
+    //checks if first input is NaN, returns error if true
     if (Number.isNaN(num1) === true){
-
         window.alert("First number is not actually a number!")
         input1.value = "";
 
+    //checks if second input is NaN, returns error if true
      } else if (Number.isNaN(num2) === true){ 
-
         console.log('ERROR')
         window.alert("Second number is not actually a number!")
         input2.value = '';
 
-
+    //if both inputs are valid, performs operation
     } else {
         if (operator.value === '+') {
-            answer.textContent = num1 + num2;
+            answer.textContent = (num1 + num2);
         } else if (operator.value === '-') {
             answer.textContent = num1 - num2;
         } else if (operator.value === '/') {
             answer.textContent = num1 / num2;
         } else if (operator.value === '*') {
-            answer.textContent = num1 * num2;
+            answer.textContent = (num1 * num2);
         }
     }
 })
