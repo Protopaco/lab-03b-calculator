8c0e510bbcf701690a131c670b7e165adba53daf
Paul Stevens <twinkisan@hotmail.com>
2020-09-23 20:44:07 -0700

    achieves basic functionality

---
+++ app.js
@@ -0,0 +1,29 @@
+// import functions and grab DOM elements
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const operator = document.getElementById('operator');
+const calcbutton = document.getElementById('calc-button');
+const answer = document.getElementById('answer');
+
+// initialize state
+
+// set event listeners to update state and DOM\
+calcbutton.addEventListener('click', () => {
+    let num1 = Number(input1.value);
+    let num2 = Number(input2.value);
+
+    console.log(operator.value)
+    if (operator.textContent === '+') {
+        answer.textContent = num1 + num2;
+    } else if (operator.value === '-') {
+        answer.textContent = num1 - num2;
+    } else if (operator.value === '/') {
+        answer.textContent = num1 / num2;
+    } else {
+        answer.textContent = num1 * num2;
+    }
+
+})
+
+
+           
